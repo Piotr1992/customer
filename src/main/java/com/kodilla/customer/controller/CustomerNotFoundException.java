b8ac1c7201dad83f8dc171a20bf7364d0d17048a
Lenovo <szyperek.piotr@o2.pl>
2021-06-11 07:11:13 +0200

    Customer ver. 1

---
+++ src/main/java/com/kodilla/customer/controller/CustomerNotFoundException.java
@@ -0,0 +1,8 @@
+package com.kodilla.customer.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("*")
+public class CustomerNotFoundException extends Exception {
+
+}
