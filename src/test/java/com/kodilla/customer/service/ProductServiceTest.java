c9afc766a05930cfe291ac9cab6ac293ae1412f3
Lenovo <szyperek.piotr@o2.pl>
2021-06-27 14:01:37 +0200

    1.7 Spring Cloud Contract - Test Customer, add directory contracts

--- src/test/java/com/kodilla/customer/service/ProductServiceTest.java
+++ src/test/java/com/kodilla/customer/service/ProductServiceTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
-import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
